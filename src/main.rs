eacbb6b764fff94f76a2438abfe850fa32b9b4d2
yuhanna <yuhannakapali@gmail.com>
2023-05-02 14:29:49 +0545

    complted the gussing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+// fn main() {
+//     println!("Hello, World!")
+// }
 
 //  requirement for the guessing game
 //  [x] 1. generate a random number
@@ -9,14 +12,42 @@
 //  [] 7. if the input is equal to the random number, print "You win!"
 
 use rand::Rng;
-
+use std::cmp;
 use std::io;
-fn main() {
-    let random_number =  rand::thread_rng().gen_range(1..101);
-    println!(" the random number is: {}", random_number );
-    println!("Input the number:");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).unwrap();
-    println!("you just entered : {}", guess)
 
+fn main() {
+    let random_number = rand::thread_rng().gen_range(1..101);
+    loop {
+        println!(" the random number is: {}", random_number);
+        println!("Input the number:");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read the data");
+
+        let quit_string = String::from(":q");
+
+        if guess.trim() == quit_string {
+            println!("You quit the game");
+            break;
+        }
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&random_number) {
+            cmp::Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+            cmp::Ordering::Greater => println!("Greater"),
+            cmp::Ordering::Less => {
+                println!("less")
+            }
+        };
+    }
 }
