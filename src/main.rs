38e6da67d442066f099651768912d4711231a414
yuhanna <yuhannakapali@gmail.com>
2023-05-19 15:24:25 +0545

    implimenting the enums on pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 #[derive(Debug)]
+enum useState {
+    Pradesh1,
+    Pradesh2,
+    Pradesh3,
+    Pradesh4,
+    Pradesh5,
+}
+
 enum Coins {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(useState),
 }
 
 fn get_value_of_coin(coin: Coins) -> u8 {
@@ -11,12 +19,15 @@ fn get_value_of_coin(coin: Coins) -> u8 {
         Coins::Penny => 1,
         Coins::Nickel => 5,
         Coins::Dime => 10,
-        Coins::Quarter => 25,
+        Coins::Quarter(state) => {
+            println!("state is {:?}", state);
+            25
+        }
     }
 }
 
 fn main() {
-    let new_coin = Coins::Quarter;
+    let new_coin = Coins::Quarter(useState::Pradesh1);
     let new_coin_value = get_value_of_coin(new_coin);
     println!("the coin value is {}", new_coin_value);
 }
