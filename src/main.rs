1461a5f4e0491af9024520f07d0bf50e4685af77
yuhanna <yuhannakapali@gmail.com>
2023-05-19 08:26:55 +0545

    added a program to use the struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+// write a program to calculate the area of rectangle using struct
+
+#[derive(Debug)]
+struct Rectangle {
+    length: i32,
+    breadth: i32,
+}
+
 fn main() {
-    println!("Hello, World!");
+    let rect1 = Rectangle {
+        length: 22,
+        breadth: 11,
+    };
+    let area = calculate_area(rect1);
+    println!("the area of rect1 is {:?}", area);
+    // println!("Hello, World!");
+}
+
+fn calculate_area(rectangle: Rectangle) -> i32 {
+    rectangle.breadth * rectangle.length
 }
