95d4e6083da1c63407fb42b2d4a46316cd52721f
yuhanna <yuhannakapali@gmail.com>
2023-05-19 11:57:51 +0545

    implimenting the enums and using them as in struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[derive(Debug)]
+enum ip_address_kind {
+    v4,
+    v6,
+}
+#[derive(Debug)]
+struct ipAddress {
+    kind: ip_address_kind,
+    address: String,
+}
+
+impl ipAddress {
+    fn new(kind: ip_address_kind, address: String) -> Self {
+        Self { kind, address }
+    }
+}
+
 fn main() {
-    println!("Hello, World!");
+    let new_ip = ipAddress::new(ip_address_kind::v4, String::from("192.168.100.1"));
+    print!("the ip is {:?}", new_ip);
 }
