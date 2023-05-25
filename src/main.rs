019acb5b7added8c0758bbabcf3017323c597915
yuhanna <yuhannakapali@gmail.com>
2023-05-25 13:16:04 +0545

    using format macro for string format

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, World!");
+    //  updating string
+    let mut string_to_modify = "नमस्ते".to_string();
+    string_to_modify.push_str(" Nepal");
+    println!("{}", string_to_modify);
+
+    // using macro to do string manipulation
+
+    let s1 = "Hello ".to_string();
+    let s2 = "World !!!".to_string();
+
+    // here + act as an a macro which work as following
+    // fn add(self, s: &str) -> String {
+    let s3 = s1 + &s2;
+
+    println!("{}", s3);
+
+    // macro for the  string
+    // format!();
+
+    let tic = String::from("Tick");
+    let tack = String::from("Tack");
+    let toe = String::from("Toe");
+
+    let s4 = format!("{tic} - {tack} - {toe}");
+    println!("{}", s4)
 }
