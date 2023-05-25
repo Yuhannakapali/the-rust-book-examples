465f2ad2bb56c2b229b175ea1cb1e02ed688c52a
yuhanna <yuhannakapali@gmail.com>
2023-05-25 11:23:43 +0545

    vector or in simple word array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, World!");
+    let mut my_mark: Vec<i32> = Vec::new();
+    my_mark.push(21);
+    my_mark.push(222);
+    // my_mark.push(-22);
+
+    let third_element = my_mark.get(2);
+
+    let trash = my_mark[0];
+
+    print!("trash is {}", trash);
+
+    match third_element {
+        Some(third) => {
+            print!("the third element is {} ", third);
+        }
+        None => {
+            print!("the third element doesn't exist")
+        }
+    }
+
+    // using macro to implement a vec
+
+    let mac_prices = vec![1200, 1000, 850];
+    //  after using you can use both get method or access using the indexes
 }
