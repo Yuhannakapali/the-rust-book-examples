36a80467c83e56c4c904bcdf90975d6564eb2bc8
yuhanna <yuhannakapali@gmail.com>
2023-05-25 11:12:33 +0545

    option and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
-#[derive(Debug)]
-enum useState {
-    Pradesh1,
-    Pradesh2,
-    Pradesh3,
-    Pradesh4,
-    Pradesh5,
-}
-
-enum Coins {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(useState),
-}
-
-fn get_value_of_coin(coin: Coins) -> u8 {
-    match coin {
-        Coins::Penny => 1,
-        Coins::Nickel => 5,
-        Coins::Dime => 10,
-        Coins::Quarter(state) => {
-            println!("state is {:?}", state);
-            25
-        }
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
 fn main() {
-    let new_coin = Coins::Quarter(useState::Pradesh1);
-    let new_coin_value = get_value_of_coin(new_coin);
-    println!("the coin value is {}", new_coin_value);
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
