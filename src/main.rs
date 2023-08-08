f8e7e641afb31a605c887319c61e84d231fc1924
Yuhanna Kapali <yuhannakapali@gmail.com>
2023-08-08 15:39:07 +0545

    added about bytes scaler and grapheme cluster

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+
 fn main() {
+
+    let mut s = "hello".to_string();
+    s.push_str(", world!");
+    println!("{}", s);
+
     //  updating string
     let mut string_to_modify = "नमस्ते".to_string();
     string_to_modify.push_str(" Nepal");
@@ -23,5 +31,47 @@ fn main() {
     let toe = String::from("Toe");
 
     let s4 = format!("{tic} - {tack} - {toe}");
-    println!("{}", s4)
+    println!("{}", s4);
+
+
+    // accessing the individual character of the string
+    let  string_to_modify = "नमस्ते".to_string();
+    
+    // bytes value
+    // [224, 164, 168, 224, 164, 174, 224, 164, 184, 224, 165, 141, 224, 164, 164,
+    // 224, 165, 135]
+
+    // scaler value
+    // ['न', 'म', 'स', '्', 'त', 'े']
+
+    // grapheme clusters
+    // ["न", "म", "स्", "ते"]
+
+
+    // accessing the individual character of the string in bytes
+    
+    print!("bytes value\n");
+    for b in string_to_modify.bytes() {
+        println!("{}", b);
+    }
+    println!("character cluter \n");
+    // accessing the individual character of the string in scaler value
+    for c in string_to_modify.chars() {
+        println!("{}", c);
+    }
+
+    // accessing the individual character of the string in grapheme clusters
+    // for g in string_to_modify.graphemes(true) {
+    //     println!("{}", g);
+    // }
+    // this doesn't work in rust too keep he code lean 
+    // we have to use external crate unicode-segmentation
+    
+    print!("Grapheme clusters: \n");
+
+    for g in string_to_modify.graphemes(true) {
+        println!("{}", g);
+    }
+
+
 }
